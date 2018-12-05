cc415ec8f239d4d24d4ede155ab0982ee7d78a5b
Michael Cumings <michael.cumings@gmail.com>
2018-12-04 21:38:09 -0600

    Updates

---
+++ precache-manifest.12f8124a3ed7d35fa434119dd9ad148a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f63d9441e58ebf6025e",
+    "url": "/clickyclicky/static/js/runtime~main.2f63d944.js"
+  },
+  {
+    "revision": "fa95c3afbe3dd8be1906",
+    "url": "/clickyclicky/static/js/main.fa95c3af.chunk.js"
+  },
+  {
+    "revision": "3ad5f8a614c5e526e94e",
+    "url": "/clickyclicky/static/js/1.3ad5f8a6.chunk.js"
+  },
+  {
+    "revision": "fa95c3afbe3dd8be1906",
+    "url": "/clickyclicky/static/css/main.230c07d7.chunk.css"
+  },
+  {
+    "revision": "d6d655867752c39b17eac4754258c8c0",
+    "url": "/clickyclicky/index.html"
+  }
+];
